b05948be1031c60c9c631337df8a6ba7d9354b38
tom <13206573871@163.com>
2019-08-06 22:18:03 +0800

    链表的奇偶排序

--- src/main/java/com/tom/list/ListOperate.java
+++ src/main/java/com/tom/list/ListOperate.java
@@ -478,4 +478,39 @@ public class ListOperate {
         return head;
     }
 
+    /**
+     * 给定一个单链表，
+     * 把所有的奇数节点和偶数节点分别排在一起。
+     * 请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+
+     * 请尝试使用原地算法完成。
+     * 你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+     *
+     * @param head
+     * @return
+     */
+    public ListNode oddEvenList(ListNode head) {
+        ListNode pre = new ListNode(-1);
+        pre.next = head;
+        ListNode odd = pre;
+        ListNode temp = new ListNode(-2);
+        ListNode tail = temp;
+        int i = 0;
+        while (odd.next != null){
+            if (i % 2 == 0){
+                odd = odd.next;
+            }else {
+                ListNode next = odd.next;
+                odd.next = next.next;
+                next.next = null;
+
+                tail.next = next;
+                tail = tail.next;
+            }
+            i++;
+        }
+        odd.next = temp.next;
+        return pre.next;
+    }
+
 }
--- src/test/java/com/tom/list/ListOperateTest.java
+++ src/test/java/com/tom/list/ListOperateTest.java
@@ -76,5 +76,15 @@ public class ListOperateTest {
 
     }
 
+    @Test
+    public void oddEvenList(){
+        int[] nums = new int[]{1,2,3,4,5,6};
+        listOperate = new ListOperate();
+        head = new ListNode(nums);
+        assertEquals(listOperate.oddEvenList(head).toString(),
+                "1->3->5->2->4->6->NULL");
+
+    }
+
 
 }
