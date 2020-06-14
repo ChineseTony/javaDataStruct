d56d14183dda3695cdec2c9a3943bd86f0f92d63
wangtao <13206573871@163.com>
2020-06-14 17:16:20 +0800

    leetcode

--- src/main/java/com/tom/oj/Solution.java
+++ src/main/java/com/tom/oj/Solution.java
@@ -1,5 +1,6 @@
 package com.tom.oj;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 /**
@@ -25,6 +26,190 @@ public class Solution {
         return result;
     }
 
+
+    public static int[] dailyTemperatures(int[] arr) {
+        //73,74,75,71,69,72,76,73
+        if(arr == null || arr.length == 0){
+            return new int[0];
+        }
+        int len = arr.length;
+        int[] result = new int[len];
+        int i = 0;
+        for (int j = 0; j < len; j++) {
+            int tmpVal = arr[j];
+            int k ;
+            for (k = i+1; k < len; k++) {
+                if(arr[k] > tmpVal){
+                    result[i++] = k - j;
+                    break;
+                }
+            }
+            if(k == len){
+                result[i++] = 0;
+            }
+        }
+        return result;
+    }
+
+
+    public static int[] dailyTemperatures2(int[] arr) {
+        if(arr == null || arr.length == 0){
+            return new int[0];
+        }
+        int len = arr.length;
+        int[] result = new int[len];
+        int i=0;
+        //保存当前元素的下标
+        Deque<Integer> stack = new ArrayDeque<> ();
+        while (i < len){
+            //把栈顶元素小于 当前元素的值
+            while (!stack.isEmpty() && arr[i] > arr[stack.peek()]){
+                    int index = stack.pop();
+                    result[index] = i - index;
+            }
+            stack.push(i);
+            i++;
+        }
+        //没有找到比当前元素大的值
+        while (!stack.isEmpty()){
+            result[stack.pop()] = 0;
+        }
+        return result;
+    }
+
+
+    public int[] sortArrayByParityII(int[] A) {
+        if(A == null){
+            return null;
+        }
+        int N = A.length;
+        int[] ans = new int[N];
+
+        int t = 0;
+        for (int x: A){
+            if (x % 2 == 0) {
+                ans[t] = x;
+                t += 2;
+            }
+        }
+
+        t = 1;
+        for (int x: A){
+            if (x % 2 == 1) {
+                ans[t] = x;
+                t += 2;
+            }
+        }
+
+        return ans;
+
+    }
+
+
+
+    public static boolean validMountainArray(int[] A) {
+        if(A == null || A.length == 0){
+            return false;
+        }
+        int len = A.length;
+        if(len == 1){
+            return false;
+        }
+        int i;
+        int index = 0;
+
+        for (i = 0; i < len-1; i++) {
+            if (A[i+1] <= A[i]){
+                index = i;
+                break;
+            }
+        }
+        if(index >= len-1 || index == 0){
+            return false;
+        }
+        for (i=index;i<len-1;i++){
+            if(A[i] <= A[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    //跟 最大子序列和思路一样 动态规划
+    public static int findLengthOfLCIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int maxLen = 0, len = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] <= nums[i]) {
+                maxLen = Math.max(maxLen, len);
+                //遇到比当前小的值 更新递增索引
+                len = 1;
+            } else {
+                len++;
+            }
+        }
+        return Math.max(maxLen, len);
+    }
+
+    public static int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int maxSum = Integer.MIN_VALUE, sum = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            if (sum < 0) {
+                sum = nums[i];
+            } else {
+                sum += nums[i];
+            }
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+
+    public static List<Integer> addToArrayForm(int[] arr, int val) {
+        int len = arr.length;
+        int lastNum =val;
+        List<Integer> ret= new LinkedList<>();
+        int i = len-1;
+        while(i >=0 || lastNum > 0){
+            //在数组范围内
+            if(i >= 0){
+                lastNum+=arr[i];
+            }
+            ret.add(0,lastNum%10);
+            lastNum/=10;
+            i--;
+        }
+        return ret;
+    }
+
+
+    public static boolean isToeplitzMatrix(int[][] matrix) {
+        if (matrix == null || matrix[0] == null){
+            return false;
+        }
+        for(int i=0;i<matrix.length-1;i++){
+            for(int j=0;j<matrix[0].length-1;j++){
+                //判断当前位置的右下角是否跟当前元素相等
+                if(matrix[i][j] != matrix[i+1][j+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
 //        int i = 6;
 //        int times = 2;
@@ -38,17 +223,37 @@ public class Solution {
 //        }
 //        printArray(a);
 
-        int[] test = new int[]{4,3,2,7,8,2,3,1};
-        List<Integer> list = findDisappearedNumbers(test);
-        for (int tmp:list){
-            System.out.print(tmp+"\t");
-        }
-        System.out.println();
-        String[] s= new String[]{
-                "testemail@leetcode.com",
-                "testemail1@leetcode.com",
-                "testemail+david@lee.tcode.com"};
-        System.out.println(numUniqueEmails(s));
+//        int[] test = new int[]{4,3,2,7,8,2,3,1};
+//        List<Integer> list = findDisappearedNumbers(test);
+//        for (int tmp:list){
+//            System.out.print(tmp+"\t");
+//        }
+//        System.out.println();
+//        String[] s= new String[]{
+//                "testemail@leetcode.com",
+//                "testemail1@leetcode.com",
+//                "testemail+david@lee.tcode.com"};
+//        System.out.println(numUniqueEmails(s));
+
+//
+//        int[] t = new int[]{1,3,4,5,7};
+//
+//        int[] result = dailyTemperatures2(t);
+//        for(int i:result){
+//            System.out.print(i+"\t");
+//        }
+//        System.out.println();
+//        System.out.println(findLengthOfLCIS(t));
+
+//        int[] arr = new int[]{9,9,9,9};
+//        System.out.println(addToArrayForm(arr,1));
+
+        int[][] arr1 = new int[][]{
+                {1,2,3},
+                {5,1,2},
+                {9,5,1}
+        };
+        System.out.println(isToeplitzMatrix(arr1));
     }
 
     public int removeDuplicates(int[] nums) {
