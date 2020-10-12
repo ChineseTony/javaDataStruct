b2de1fd82d513cef5075f81f2e74c8a1f1f9c8d9
wangtao01 <13206573871@163.com>
2020-10-12 17:10:57 +0800

    binary search

--- src/main/java/com/tom/array/SearchTarget.java
+++ src/main/java/com/tom/array/SearchTarget.java
@@ -106,12 +106,52 @@ public class SearchTarget {
     }
 
 
+
+//    https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+    public static int findMin(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        int left = 0;
+        int right = len -1;
+        //有序
+        if (nums[right] > nums[0]) {
+            return nums[0];
+        }
+        while (left <= right){
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            if (nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            }
+            if (nums[mid] > nums[0]){
+               left = mid+1;
+            }else{
+                right =mid-1;
+            }
+        }
+        return -1;
+    }
+
+
+
+
     public static void main(String[] args) {
         int[] arr = new int[]{4,5,6,7,0,1,2};
         System.out.println(search2(arr,0));
 
         int[] arr2 = new int[]{0,0,0,1,1,2};
         System.out.println(findRightValue(arr2,1));
+
+
+        int[] result = new int[]{2,3,4,5,1};
+        System.out.println(findMin(result));
 //        System.out.println(search2(arr,3));
     }
 }
