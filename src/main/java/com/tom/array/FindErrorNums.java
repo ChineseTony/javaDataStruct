26f0b728806100285c61e619b8524cfe78303125
wangtao01 <13206573871@163.com>
2020-10-19 16:30:50 +0800

    leetcode

---
+++ src/main/java/com/tom/array/FindErrorNums.java
@@ -0,0 +1,43 @@
+package com.tom.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * @link https://leetcode-cn.com/problems/set-mismatch/
+ */
+public class FindErrorNums {
+
+    private FindErrorNums(){
+
+    }
+
+
+
+    public int[] findErrorNums(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return nums;
+        }
+        int len = nums.length;
+        Set<Integer> set = new HashSet<>(len);
+        int sum = 0;
+        int tmp = 0;
+        int repeat = 0;
+        for (int i = 0; i < len; i++) {
+            if (set.contains(nums[i])){
+                repeat = nums[i];
+            }
+            tmp += i+1;
+            sum += nums[i];
+            set.add(nums[i]);
+        }
+        int[] result = new int[2];
+        result[0] = repeat;
+        result[1] = repeat + tmp - sum;
+        return result;
+    }
+
+
+
+}
