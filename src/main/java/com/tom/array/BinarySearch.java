52c969a8603ba76eafedfe53fabf233abdd167ed
wangtao01 <13206573871@163.com>
2020-10-27 15:11:25 +0800

    leetcode

--- src/main/java/com/tom/array/BinarySearch.java
+++ src/main/java/com/tom/array/BinarySearch.java
@@ -97,6 +97,32 @@ public class BinarySearch<T extends Comparable<T>> {
         return -1;
     }
 
+
+    /**
+     * [2,1,1,2]
+     * 2 + 2 = 4
+     * @param nums
+     * @return
+     * @link https://leetcode-cn.com/problems/the-masseuse-lcci/
+     */
+    public int massage(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int dp0 = 0, dp1 = nums[0];
+
+        for (int i = 1; i < n; ++i){
+            int tdp0 = Math.max(dp0, dp1); // 计算 dp[i][0]
+            int tdp1 = dp0 + nums[i]; // 计算 dp[i][1]
+
+            dp0 = tdp0; // 用 dp[i][0] 更新 dp_0
+            dp1 = tdp1; // 用 dp[i][1] 更新 dp_1
+        }
+        return Math.max(dp0, dp1);
+
+    }
+
     public static void main(String[] args) {
         BinarySearch<Integer> binarySearch = new BinarySearch<>();
         Integer[] tmp = new Integer[]{1,2,4,5,5,5,6,7,10};
