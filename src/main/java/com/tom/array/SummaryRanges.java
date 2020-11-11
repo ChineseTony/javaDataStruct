795ebd4d1a6b62e91e2ddf2905afc296e4f8a30b
wangtao01 <13206573871@163.com>
2020-11-11 11:06:55 +0800

    leetcode

--- src/main/java/com/tom/array/SummaryRanges.java
+++ src/main/java/com/tom/array/SummaryRanges.java
@@ -1,9 +1,7 @@
 package com.tom.array;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 
 public class SummaryRanges {
 
--- src/main/java/com/tom/string/LongPressedName.java
+++ src/main/java/com/tom/string/LongPressedName.java
@@ -68,11 +68,74 @@ public class LongPressedName {
         return tmp -1 ;
     }
 
+
+    /**
+     * 字符串求和
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+        StringBuffer ans = new StringBuffer();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+        }
+        // 计算完以后的答案需要翻转过来
+        ans.reverse();
+        return ans.toString();
+
+    }
+
+
+    /**
+     * @link https://leetcode-cn.com/problems/student-attendance-record-i/
+     * @param s
+     * @return
+     * "PPALLPLLP"
+     * true
+     */
+    public static boolean checkRecord(String s) {
+        if (s == null || s.length() == 0){
+            return true;
+        }
+        int len = s.length();
+        int count = 0;
+        int i = 0;
+        while (i < len) {
+            if (s.charAt(i) == 'A'){
+                count++;
+            }else if (s.charAt(i) == 'L'){
+                int tmp = 1;
+                while (i < len-1 && s.charAt(i+1) == 'L'){
+                    tmp++;
+                    if (tmp > 2){
+                        return false;
+                    }
+                    i++;
+                }
+            }
+            i++;
+            if (count > 1){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(isLongPressedName("leelee","lleeelee"));
 
         int[] nums = new int[]{2,3,4,7,11};
 //缺失的正整数包括 [1,5,6,8,9,10,12,13,...]
         System.out.println(findKthPositive(nums,8));
+
+        System.out.println(checkRecord("PLLL"));
     }
 }
