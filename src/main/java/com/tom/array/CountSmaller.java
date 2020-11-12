a5895f6cf3fc652ac4a7790ee3e825ff83727830
wangtao01 <13206573871@163.com>
2020-11-12 10:31:51 +0800

    leetcode

--- src/main/java/com/tom/array/CountSmaller.java
+++ src/main/java/com/tom/array/CountSmaller.java
@@ -1,7 +1,9 @@
 package com.tom.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class CountSmaller {
 
@@ -55,6 +57,51 @@ public class CountSmaller {
         return result;
     }
 
+    public static int[] sortArrayByParityII(int[] arr) {
+        int n = arr.length;
+        int j = 1;
+        for (int i = 0; i < n; i += 2) {
+            if (arr[i] % 2 == 1) {
+                while (arr[j] % 2 == 1) {
+                    j += 2;
+                }
+                swap(arr, i, j);
+            }
+        }
+        return arr;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+
+    public int[] sortArrayByParityII2(int[] A) {
+        if(A == null){
+            return null;
+        }
+        int N = A.length;
+        int[] ans = new int[N];
+        int t = 0;
+        for (int x: A){
+            if (x % 2 == 0) {
+                ans[t] = x;
+                t += 2;
+            }
+        }
+        t = 1;
+        for (int x: A){
+            if (x % 2 == 1) {
+                ans[t] = x;
+                t += 2;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{5,2,6,1};
         List<Integer> result = countSmaller(arr);
@@ -62,6 +109,10 @@ public class CountSmaller {
             System.out.print(i+"\t");
         }
         System.out.println();
+        int[] tmp = new int[]{2,3,1,1,4,0,0,4,3,3};
+        Arrays.stream(sortArrayByParityII(tmp)).forEach(
+                System.out::println
+        );
 
     }
 }
