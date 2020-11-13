61d5d8c2d1c2773ab096f25660898bf2ec098609
wangtao01 <13206573871@163.com>
2020-11-13 10:54:18 +0800

    leetcode

--- src/main/java/com/tom/array/MaxValueArray.java
+++ src/main/java/com/tom/array/MaxValueArray.java
@@ -53,6 +53,38 @@ public class MaxValueArray {
         return max[col-1];
     }
 
+
+    /**
+     * 主要考察 桶排序算法
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        //第一步：将arr1中数据分配到各桶中
+        int[] bucket=new int[1001];
+        int[] res=new int[arr1.length];
+        for (int data:arr1) {
+            bucket[data]++;
+        }
+        //第二步：按照arr2的顺序收集桶中数据
+        int j=0;
+        for (int data:arr2){
+            //在arr2中的数据 循环遍历 bucket 变成0
+            while (bucket[data]-->0) {
+                res[j++] = data;
+            }
+        }
+        //第三步：按升序顺序收集桶中其他数据
+        for (int i=0;i<1001;i++){
+            while (bucket[i]-->0) {
+                res[j++] = i;
+            }
+        }
+        return res;
+
+    }
+
     public static void main(String[] args) {
         int[][] arr = new int[2][4];
         int count=0;
