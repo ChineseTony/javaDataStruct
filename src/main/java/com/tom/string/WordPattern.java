f1ab985f0e91cff81bf4de24901ef6e09f2948ac
wangtao01 <13206573871@163.com>
2020-11-17 11:12:21 +0800

    leetcode

--- src/main/java/com/tom/string/WordPattern.java
+++ src/main/java/com/tom/string/WordPattern.java
@@ -34,10 +34,83 @@ public class WordPattern {
         return true;
     }
 
+
+    public String reverseWords(String s) {
+        if(s == null || s.length() == 0){
+            return s;
+        }
+
+        String[] tmp = s.split(" ");
+        int len = tmp.length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            sb.append(reverse(tmp[i]));
+            if (i != len-1){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+
+
+    public  String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+
+    /**
+     * @link https://leetcode-cn.com/problems/check-if-a-w
+     * ord-occurs-as-a-prefix-of-any-word-in-a-sentence/
+     * @param sentence
+     * @param searchWord
+     * @return
+     */
+    public static int isPrefixOfWord(String sentence, String searchWord) {
+        if(sentence == null || sentence.length() == 0){
+            return -1;
+        }
+        if(searchWord == null || searchWord.length() == 0){
+            return -1;
+        }
+        String[] tmp = sentence.split(" ");
+        int len = tmp.length;
+        for (int i = 0; i < len; i++) {
+            String string = tmp[i];
+            if (isPrefixOf(string,searchWord)){
+                return i+1;
+            }
+
+        }
+        return -1;
+    }
+
+    private static boolean isPrefixOf(String str,String searchWord){
+        int len1 = str.length();
+        int len2 = searchWord.length();
+        int i = 0;
+        while (i < len1 && i < len2){
+            if (str.charAt(i) != searchWord.charAt(i)){
+                return false;
+            }
+            i++;
+        }
+        if (i == len2 || len1 == len2){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
     public static void main(String[] args) {
         String pattern  = "abba";
         String str = "dog cat cat fish";
         System.out.println(wordPattern(pattern,str));
+
+
+        String sentence  = "i love eating burger";
+        String searchWord = "burg";
+        System.out.println(isPrefixOfWord(sentence,searchWord));
 
     }
 }
