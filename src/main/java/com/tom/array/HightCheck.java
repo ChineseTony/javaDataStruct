c66880ed4171ce41f80f95ad73773692018974c1
wangtao01 <13206573871@163.com>
2020-11-27 21:06:41 +0800

    leetcode

--- src/main/java/com/tom/array/HightCheck.java
+++ src/main/java/com/tom/array/HightCheck.java
@@ -1,6 +1,6 @@
 package com.tom.array;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @author WangTao
@@ -47,8 +47,70 @@ public class HightCheck {
     }
 
 
+    public static int[] frequencySort(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return nums;
+        }
+        int len = nums.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            if (!map.containsKey(nums[i])){
+                map.put(nums[i],1);
+            }else {
+                map.put(nums[i],map.get(nums[i]) + 1);
+            }
+        }
+        List<Fre> freList = new ArrayList<>();
+        for (Map.Entry<Integer,Integer> entry:map.entrySet()){
+            freList.add(new Fre(entry.getKey(),entry.getValue()));
+        }
+        freList.sort((o1, o2) -> {
+            if (o1.value > o2.value){
+                return 1;
+            } else if (o1.value < o2.value) {
+                return -1;
+            }else {
+                if (o1.key > o2.key){
+                    return -1;
+                }else if (o1.key < o2.key){
+                    return 1;
+                }else {
+                    return 0;
+                }
+            }
+        });
+        int i = 0;
+        for (Fre fre:freList){
+            while (fre.value -- >0){
+                nums[i++] = fre.key;
+            }
+        }
+        return nums;
+    }
+
+    static class Fre{
+        public int key;
+        public int value;
+
+        public Fre(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Fre{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+
     public static void main(String[] args) {
         int[] a = new int[]{1,1,4,2,1,3};
         System.out.println(heightChecker(a));
+        int[] nums = new int[]{2,3,1,3,2};
+        Arrays.stream(frequencySort(nums)).forEach(System.out::println);
     }
 }
