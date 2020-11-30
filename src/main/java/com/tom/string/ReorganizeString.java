cf0f9aad72215f447afceeaba46c6fa2d9ccd3cb
wangtao01 <13206573871@163.com>
2020-11-30 12:58:42 +0800

    leetcode

---
+++ src/main/java/com/tom/string/ReorganizeString.java
@@ -0,0 +1,62 @@
+package com.tom.string;
+
+
+import java.util.*;
+
+public class ReorganizeString {
+
+    private ReorganizeString(){
+
+    }
+
+
+    public static String reorganizeString(String S) {
+        if (S.length() < 2) {
+            return S;
+        }
+        int[] counts = new int[26];
+        int maxCount = 0;
+        int length = S.length();
+        for (int i = 0; i < length; i++) {
+            char c = S.charAt(i);
+            counts[c - 'a']++;
+            maxCount = Math.max(maxCount, counts[c - 'a']);
+        }
+        if (maxCount > (length + 1) / 2) {
+            return "";
+        }
+        PriorityQueue<Character> queue = new PriorityQueue<>(
+                (letter1, letter2) -> counts[letter2 - 'a'] - counts[letter1 - 'a']);
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (counts[c - 'a'] > 0) {
+                queue.offer(c);
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        while (queue.size() > 1) {
+            char letter1 = queue.poll();
+            char letter2 = queue.poll();
+            sb.append(letter1);
+            sb.append(letter2);
+            int index1 = letter1 - 'a', index2 = letter2 - 'a';
+            counts[index1]--;
+            counts[index2]--;
+            if (counts[index1] > 0) {
+                queue.offer(letter1);
+            }
+            if (counts[index2] > 0) {
+                queue.offer(letter2);
+            }
+        }
+        if (queue.size() > 0) {
+            sb.append(queue.poll());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+//        String tmp = "aaacc";
+        String tmp = "vvvlo";
+        System.out.println(reorganizeString(tmp));
+    }
+}
