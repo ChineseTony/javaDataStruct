5bb8cb71b79ed4176d845e54fbcdd16bfc35defb
wangtao01 <13206573871@163.com>
2020-12-01 15:26:48 +0800

    leetcode

--- src/main/java/com/tom/string/LargestNumber.java
+++ src/main/java/com/tom/string/LargestNumber.java
@@ -38,9 +38,33 @@ public class LargestNumber {
 
 
 
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int  count = 0;
+        if(nums == null || nums.length == 0){
+            return count;
+        }
+        int len = nums.length;
+        int result = 0;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == 1){
+                count++;
+            }else {
+                result = Math.max(result,count);
+                count = 0;
+            }
+        }
+        result = Math.max(result,count);
+        return result;
+    }
+
+
     public static void main(String[] args) {
         int[] tmp = new int[]{34323,3432};
         System.out.println(largestNumber(tmp));
+
+        int[] arr = new int[]{1,0,1,1,0,1};
+
+        System.out.println(findMaxConsecutiveOnes(arr));
 
     }
 }
