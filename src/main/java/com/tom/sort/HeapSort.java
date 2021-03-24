aba309bd0ff837f6c7c79877be9fba50cc78105d
“ChineseTony“ <13206573871@163.com>
2021-03-24 18:09:42 +0800

    min heap

--- src/main/java/com/tom/sort/HeapSort.java
+++ src/main/java/com/tom/sort/HeapSort.java
@@ -9,8 +9,8 @@ public class HeapSort <S extends Comparable<S>> implements Sort<S> {
 
 
     /**
-     * 堆排序思路 讲无序数字 构建为一个大根堆 把根元素放到数组末尾
-     * 然后再将原有元素 重新构建大根堆
+     * 堆排序思路 将无序数字 构建为一个大根堆 把根元素放到数组末尾
+     * 然后从数组末尾到数组开头
      * @param t 排序数组
      */
     @Override
@@ -50,6 +50,31 @@ public class HeapSort <S extends Comparable<S>> implements Sort<S> {
 
     }
 
+
+    public void sort2(S[] t){
+        int len = t.length;
+        //从最后一个节点进行上沉操作 heapify 堆化
+        for (int i = len-1; i >= 0 ; i--) {
+            shiftUp(t,i);
+        }
+        for(int j=len - 1;j>0;j--){
+            //将堆顶元素与末尾元素进行交换
+            swap(t,0,j);
+            //重新对堆进行调整
+            for (int i = j-1; i > 0 ; i--) {
+                shiftUp(t,i);
+            }
+        }
+    }
+
+    private void shiftUp(S[] t, int i) {
+        while (i > 0 && t[i].compareTo(t[getParent(i)]) < 0){
+            swap(t,getParent(i),i);
+            i = getParent(i);
+        }
+
+    }
+
     private int getLeftChild(int i){
         return 2 * i + 1;
     }
@@ -66,6 +91,7 @@ public class HeapSort <S extends Comparable<S>> implements Sort<S> {
         System.out.println("排序前："+ Arrays.toString(arr));
         new HeapSort<Integer>().sort(arr);
         System.out.println("排序后："+Arrays.toString(arr));
-
+        new HeapSort<Integer>().sort2(arr);
+        System.out.println("排序后："+Arrays.toString(arr));
     }
 }
